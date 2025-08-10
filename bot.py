d9fd67bd877c6c3c3c0d856b76fd522565b7ddb6
Yunus Azimli <afsananamazova01@gmail.com>
2025-08-10 13:36:06 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import telebot # библиотека telebot
+import sys
+import os
+sys.path.append(os.getcwd())
 from config import token # импорт токена
 
 bot = telebot.TeleBot(token) 
@@ -7,6 +10,11 @@ bot = telebot.TeleBot(token)
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
+@bot.message_handler(commands=['info'])
+def info(message):
+    bot.reply_to(message, "Мои команды: /start ,/ban ,info")
+    bot.reply_to(message, "Используйте меня с умом")
+    
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
     if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
@@ -23,4 +31,26 @@ def ban_user(message):
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
-bot.infinity_polling(none_stop=True)
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    if "https://" in message.text:
+        user_info = {
+            "chat_id": message.chat.id,
+            "user_id": message.from_user.id,
+            "username": message.from_user.username,
+            "first_name": message.from_user.first_name,
+            "last_name": message.from_user.last_name,
+            "text": message.text
+        }
+        bot.reply_to(message, f"Пользователь с ссылкой - {user_info}")
+        try:
+            bot.ban_chat_member(message.chat.id, message.from_user.id)
+            bot.reply_to(message, f"""Пользователь @{message.reply_to_message.from_user.username} был забанен по причине отправки ссылок.
+Отправлять ссылки в данный чат  КАТЕГОРИЧЕСКИ ЗАПРЕЩЕНО !!!!!""")
+        except Exception as e:
+            bot.reply_to(message, f"Не удалось забанить пользователя: {e}")
+    else:    
+        bot.reply_to(message, message.text) 
+if __name__ == "__main__":
+    print("Бот запустился...")
+    bot.polling()
