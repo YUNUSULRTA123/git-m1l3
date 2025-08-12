6426bdd7dc3c46e02f88d5c52ab0b7ba1879f147
Yunus Azimli <afsananamazova01@gmail.com>
2025-08-12 16:47:41 +0400

    Update

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ def start(message):
 
 @bot.message_handler(commands=['info'])
 def info(message):
-    bot.reply_to(message, "Мои команды: /start ,/ban ,/info")
+    bot.reply_to(message, "Мои команды: /start ,/ban ,/info, /unban")
     bot.reply_to(message, "Используйте меня с умом")
     
 @bot.message_handler(commands=['ban'])
@@ -28,6 +28,22 @@ def ban_user(message):
         else:
             bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+
+@bot.message_handler(commands=['unban'])
+def unban_user(message):
+    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
+        chat_id = message.chat.id # сохранение id чата
+         # сохранение id и статуса пользователя, отправившего сообщение
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+         # проверка пользователя
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить или же разбанить администратора.")
+        else:
+            bot.unban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был разбанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
